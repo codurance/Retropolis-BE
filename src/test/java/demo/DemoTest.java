e3a4eee535392023e0d88c073a2cd4ad88199287
Ewan <ewan.s@cucumberwifi.io>
2020-05-12 13:46:10 +0100

    initial commit

---
+++ src/test/java/demo/DemoTest.java
@@ -0,0 +1,12 @@
+package demo;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class DemoTest {
+    @Test
+    void dummy() {
+        assertTrue(true);
+    }
+}
