bdd86d62e4d0c616102d1c0da2b985acd7257021
Ewan <ewan.s@cucumberwifi.io>
2020-05-14 09:57:18 +0100

    remove demo test

--- src/main/java/com/codurance/retropolis/controllers/CardController.java
+++ src/main/java/com/codurance/retropolis/controllers/CardController.java
@@ -5,12 +5,9 @@ import com.codurance.retropolis.requests.NewCardRequestObject;
 import com.codurance.retropolis.services.CardService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-
-import static org.springframework.http.ResponseEntity.status;
 
 @RestController
 @RequestMapping("/cards")
