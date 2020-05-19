89a7c21cbb351cfc4623a0255f697022dc339fb4
Alexis Pavlidis <apavlidi@it.teithe.gr>
2020-05-19 13:09:43 +0300

    refactor execute post to be agnostic of the request

--- src/test/java/com/codurance/retropolis/acceptance/HttpHelpers.java
+++ src/test/java/com/codurance/retropolis/acceptance/HttpHelpers.java
@@ -1,8 +1,5 @@
 package com.codurance.retropolis.acceptance;
 
-import com.codurance.retropolis.factories.CardFactory;
-import com.codurance.retropolis.models.Card;
-import com.codurance.retropolis.requests.NewCardRequestObject;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
@@ -19,12 +16,8 @@ public class HttpHelpers {
     getResponse = restTemplate.execute(url, HttpMethod.GET, null, ResponseResults::new);
   }
 
-  public static void executePost(String url, int columnId, String text) {
-    RestTemplate restTemplate = new RestTemplate();
-    CardFactory cardFactory = new CardFactory();
-    Card card = cardFactory.create(new NewCardRequestObject(text, columnId));
-    HttpEntity<Card> request = new HttpEntity<>(card);
+  public static void executePost(String url, HttpEntity<?> request, Class<?> responseType) {
     postResponse = restTemplate
-        .exchange(url, HttpMethod.POST, request, Card.class);
+        .exchange(url, HttpMethod.POST, request, responseType);
   }
 }
--- src/test/java/com/codurance/retropolis/acceptance/cards/CardStepDefsIntegrationTest.java
+++ src/test/java/com/codurance/retropolis/acceptance/cards/CardStepDefsIntegrationTest.java
@@ -6,15 +6,18 @@ import static org.hamcrest.Matchers.is;
 
 import com.codurance.retropolis.acceptance.HttpHelpers;
 import com.codurance.retropolis.factories.CardFactory;
+import com.codurance.retropolis.models.Card;
 import com.codurance.retropolis.requests.NewCardRequestObject;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.When;
+import org.springframework.http.HttpEntity;
 
 public class CardStepDefsIntegrationTest {
 
   @When("the client posts to cards endpoint with column_id:{int} and text:{string}")
   public void theClientPostsToCardsEndpointWithColumn_idAndText(int columnId, String text) {
-    HttpHelpers.executePost("http://localhost:5000/cards", columnId, text);
+    Card card = new CardFactory().create(new NewCardRequestObject(text, columnId));
+    HttpHelpers.executePost("http://localhost:5000/cards", new HttpEntity<>(card), Card.class);
   }
 
   @And("the client receives the card with the column_id:{int} and text:{string}")
