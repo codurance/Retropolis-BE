03a76ad6cc9d94df639b986d222de68e301a4357
Alexis Pavlidis <apavlidi@it.teithe.gr>
2020-05-21 18:57:00 +0300

    fix properties

--- src/main/java/com/codurance/retropolis/Retropolis.java
+++ src/main/java/com/codurance/retropolis/Retropolis.java
@@ -3,10 +3,7 @@ package com.codurance.retropolis;
 import com.codurance.retropolis.config.Environment;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 @ActiveProfiles(Environment.PROD)
@@ -14,16 +11,6 @@ public class Retropolis {
 
   public static void main(String[] args) {
     SpringApplication.run(Retropolis.class, args);
-  }
-
-  @Bean
-  public WebMvcConfigurer corsConfigurer() {
-    return new WebMvcConfigurer() {
-      @Override
-      public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**").allowedOrigins("*");
-      }
-    };
   }
 
 }
