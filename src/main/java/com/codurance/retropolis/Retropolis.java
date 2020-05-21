052c6799b9d64c556fbc4f76c2532371397bc2d8
Morinslash <sebastian.elf.kulinski@gmail.com>
2020-05-21 17:30:11 +0200

    add MvcCors config

--- src/main/java/com/codurance/retropolis/Retropolis.java
+++ src/main/java/com/codurance/retropolis/Retropolis.java
@@ -3,7 +3,10 @@ package com.codurance.retropolis;
 import com.codurance.retropolis.config.Environment;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 @ActiveProfiles(Environment.PROD)
@@ -11,6 +14,16 @@ public class Retropolis {
 
   public static void main(String[] args) {
     SpringApplication.run(Retropolis.class, args);
+  }
+
+  @Bean
+  public WebMvcConfigurer corsConfigurer() {
+    return new WebMvcConfigurer() {
+      @Override
+      public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**").allowedOrigins("*");
+      }
+    };
   }
 
 }
--- src/main/java/com/codurance/retropolis/config/WebConfig.java
+++ src/main/java/com/codurance/retropolis/config/WebConfig.java
@@ -3,7 +3,6 @@ package com.codurance.retropolis.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -19,11 +18,11 @@ public class WebConfig {
     @Autowired
     private GoogleTokenAuthenticator googleTokenAuthenticator;
 
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-      registry.addMapping("/**")
-          .allowedOrigins("http://retropolis-fe.s3-website.eu-west-2.amazonaws.com");
-    }
+//    @Override
+//    public void addCorsMappings(CorsRegistry registry) {
+//      registry.addMapping("/**")
+//          .allowedOrigins("http://retropolis-fe.s3-website.eu-west-2.amazonaws.com");
+//    }
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
