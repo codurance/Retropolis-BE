190930a70071524e65dee13ce61b37cea9a4a5e7
Morinslash <sebastian.elf.kulinski@gmail.com>
2020-05-22 10:29:06 +0200

    remove slash at the end

--- src/main/java/com/codurance/retropolis/Retropolis.java
+++ src/main/java/com/codurance/retropolis/Retropolis.java
@@ -10,30 +10,30 @@ import org.springframework.test.context.ActiveProfiles;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
+
 import java.util.Arrays;
 import java.util.Collections;
-import org.springframework.test.context.ActiveProfiles;
 
 
 @SpringBootApplication
 @ActiveProfiles(Environment.PROD)
 public class Retropolis {
 
-  public static void main(String[] args) {
-    SpringApplication.run(Retropolis.class, args);
-  }
+    public static void main(String[] args) {
+        SpringApplication.run(Retropolis.class, args);
+    }
 
-  @Bean
-  public FilterRegistrationBean<CorsFilter> simpleCorsFilter() {
-    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-    CorsConfiguration config = new CorsConfiguration();
-    config.setAllowCredentials(true);
-    config.setAllowedOrigins(Arrays.asList("http://localhost:3000","http://localhost:5000", "http://retropolis-fe.s3-website.eu-west-2.amazonaws.com/"));
-    config.setAllowedMethods(Collections.singletonList("*"));
-    config.setAllowedHeaders(Collections.singletonList("*"));
-    source.registerCorsConfiguration("/**", config);
-    FilterRegistrationBean<CorsFilter> bean = new FilterRegistrationBean<>(new CorsFilter(source));
-    bean.setOrder(Ordered.HIGHEST_PRECEDENCE);
-    return bean;
-  }
+    @Bean
+    public FilterRegistrationBean<CorsFilter> simpleCorsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.setAllowedOrigins(Arrays.asList("http://localhost:3000", "http://localhost:5000", "http://retropolis-fe.s3-website.eu-west-2.amazonaws.com"));
+        config.setAllowedMethods(Collections.singletonList("*"));
+        config.setAllowedHeaders(Collections.singletonList("*"));
+        source.registerCorsConfiguration("/**", config);
+        FilterRegistrationBean<CorsFilter> bean = new FilterRegistrationBean<>(new CorsFilter(source));
+        bean.setOrder(Ordered.HIGHEST_PRECEDENCE);
+        return bean;
+    }
 }
