0109fbb99ef069668cf594c8261adcd28466d295
Alexis Pavlidis <apavlidi@it.teithe.gr>
2020-05-22 13:00:54 +0300

    add authentication with hardcoded client id

--- src/main/java/com/codurance/retropolis/config/WebConfig.java
+++ src/main/java/com/codurance/retropolis/config/WebConfig.java
@@ -1,12 +1,15 @@
 package com.codurance.retropolis.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @EnableWebMvc
+@Profile("!" + Environment.TEST)
 public class WebConfig {
 
   @Configuration
@@ -16,6 +19,11 @@ public class WebConfig {
     public void addCorsMappings(CorsRegistry registry) {
       registry.addMapping("/**")
           .allowedOrigins("http://retropolis-fe.s3-website.eu-west-2.amazonaws.com");
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+      registry.addInterceptor(new GoogleTokenAuthenticator());
     }
   }
 }
