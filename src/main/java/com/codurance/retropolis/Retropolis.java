18bff51aee27424c39f4d72f61429bd2dcdbaf42
Alexis Pavlidis <apavlidi@it.teithe.gr>
2020-05-22 13:06:18 +0300

    add filter cors

--- src/main/java/com/codurance/retropolis/Retropolis.java
+++ src/main/java/com/codurance/retropolis/Retropolis.java
@@ -1,14 +1,40 @@
 package com.codurance.retropolis;
 
+import com.codurance.retropolis.config.Environment;
+import java.util.Arrays;
+import java.util.Collections;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.Ordered;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 
 @SpringBootApplication
+@ActiveProfiles(Environment.PROD)
 public class Retropolis {
 
     public static void main(String[] args) {
         SpringApplication.run(Retropolis.class, args);
     }
 
+    @Bean
+    public FilterRegistrationBean<CorsFilter> simpleCorsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+//        config.setAllowedOrigins(Arrays.asList("*"));
+        config.setAllowedOrigins(Arrays
+            .asList("http://localhost:3000", "http://localhost:5000", "http://retropolis-fe.s3-website.eu-west-2.amazonaws.com"));
+        config.setAllowedMethods(Collections.singletonList("*"));
+        config.setAllowedHeaders(Collections.singletonList("*"));
+        source.registerCorsConfiguration("/**", config);
+        FilterRegistrationBean<CorsFilter> bean = new FilterRegistrationBean<>(new CorsFilter(source));
+        bean.setOrder(Ordered.HIGHEST_PRECEDENCE);
+        return bean;
+    }
 }
