9fb3c265229a4cb5c51450124efdad577d9d5e1b
Ewan <ewan.s@cucumberwifi.io>
2020-05-28 17:58:17 +0100

    schema change and revert configs

--- src/main/java/com/codurance/retropolis/Retropolis.java
+++ src/main/java/com/codurance/retropolis/Retropolis.java
@@ -24,8 +24,7 @@ public class Retropolis {
     UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
     CorsConfiguration config = new CorsConfiguration();
     config.setAllowCredentials(true);
-    config.setAllowedOrigins(
-        List.of("http://retropolis-fe.s3-website.eu-west-2.amazonaws.com", "http://localhost:3000"));
+    config.setAllowedOrigins(Collections.singletonList("http://retropolis-fe.s3-website.eu-west-2.amazonaws.com"));;
     config.setAllowedMethods(Collections.singletonList("*"));
     config.setAllowedHeaders(Collections.singletonList("*"));
     source.registerCorsConfiguration("/**", config);
