fd425da9e78d32aa6359f151d79f854506daf725
Alexis Pavlidis <apavlidi@it.teithe.gr>
2020-05-29 16:57:39 +0300

    revert cors filtering on springboot class

--- src/main/java/com/codurance/retropolis/config/WebConfig.java
+++ src/main/java/com/codurance/retropolis/config/WebConfig.java
@@ -1,21 +1,14 @@
 package com.codurance.retropolis.config;
 
-import java.util.Collections;
-import org.springframework.boot.web.servlet.FilterRegistrationBean;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
-import org.springframework.core.Ordered;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.filter.CorsFilter;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @EnableWebMvc
-@Profile(Environment.PROD)
+@Profile("!" + Environment.TEST)
 public class WebConfig {
 
   @Configuration
@@ -25,19 +18,5 @@ public class WebConfig {
     public void addInterceptors(InterceptorRegistry registry) {
       registry.addInterceptor(new GoogleTokenAuthenticator());
     }
-  }
-
-  @Bean
-  public FilterRegistrationBean<CorsFilter> simpleCorsFilter() {
-    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-    CorsConfiguration config = new CorsConfiguration();
-    config.setAllowCredentials(true);
-    config.setAllowedOrigins(Collections.singletonList("http://localhost:3000"));
-    config.setAllowedMethods(Collections.singletonList("*"));
-    config.setAllowedHeaders(Collections.singletonList("*"));
-    source.registerCorsConfiguration("/**", config);
-    FilterRegistrationBean<CorsFilter> bean = new FilterRegistrationBean<>(new CorsFilter(source));
-    bean.setOrder(Ordered.HIGHEST_PRECEDENCE);
-    return bean;
   }
 }
