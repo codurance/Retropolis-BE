ecf9809c0c6700ac3ec05f22d6ade6d5bea49a00
Alexis Pavlidis <apavlidi@it.teithe.gr>
2020-05-29 16:52:13 +0300

    remove duplicate cors, move cors filterting

--- src/main/java/com/codurance/retropolis/config/WebConfig.java
+++ src/main/java/com/codurance/retropolis/config/WebConfig.java
@@ -1,29 +1,43 @@
 package com.codurance.retropolis.config;
 
+import java.util.Collections;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.core.Ordered;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @EnableWebMvc
-@Profile("!" + Environment.TEST)
+@Profile(Environment.PROD)
 public class WebConfig {
 
   @Configuration
   public class WebConfiguration implements WebMvcConfigurer {
 
     @Override
-    public void addCorsMappings(CorsRegistry registry) {
-      registry.addMapping("/**")
-          .allowedOrigins("http://retropolis-fe.s3-website.eu-west-2.amazonaws.com");
-    }
-
-    @Override
     public void addInterceptors(InterceptorRegistry registry) {
       registry.addInterceptor(new GoogleTokenAuthenticator());
     }
+  }
+
+  @Bean
+  public FilterRegistrationBean<CorsFilter> simpleCorsFilter() {
+    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+    CorsConfiguration config = new CorsConfiguration();
+    config.setAllowCredentials(true);
+    config.setAllowedOrigins(Collections.singletonList("http://localhost:3000"));
+    config.setAllowedMethods(Collections.singletonList("*"));
+    config.setAllowedHeaders(Collections.singletonList("*"));
+    source.registerCorsConfiguration("/**", config);
+    FilterRegistrationBean<CorsFilter> bean = new FilterRegistrationBean<>(new CorsFilter(source));
+    bean.setOrder(Ordered.HIGHEST_PRECEDENCE);
+    return bean;
   }
 }
