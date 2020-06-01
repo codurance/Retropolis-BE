59e990056de96413e8d691599221896c51554ebb
Ewan <ewan.s@cucumberwifi.io>
2020-06-01 14:55:55 +0100

    adds step definitions for fetching boards acceptance test

--- src/test/java/com/codurance/retropolis/acceptance/board/BoardStepDefsIntegrationTest.java
+++ src/test/java/com/codurance/retropolis/acceptance/board/BoardStepDefsIntegrationTest.java
@@ -1,13 +1,20 @@
 package com.codurance.retropolis.acceptance.board;
 
 import static com.codurance.retropolis.utils.Convert.asJsonString;
+import static com.codurance.retropolis.utils.HttpWrapper.executePost;
+import static com.codurance.retropolis.utils.HttpWrapper.responseResult;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 
 import com.codurance.retropolis.acceptance.BaseStepDefinition;
 import com.codurance.retropolis.models.Board;
+import com.codurance.retropolis.models.Card;
 import com.codurance.retropolis.models.Column;
+import com.codurance.retropolis.requests.NewCardRequestObject;
 import com.codurance.retropolis.utils.HttpWrapper;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import io.cucumber.java.Before;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
@@ -17,6 +24,7 @@ import java.sql.SQLException;
 import java.util.Collections;
 import java.util.List;
 import javax.sql.DataSource;
+import org.springframework.http.HttpEntity;
 
 public class BoardStepDefsIntegrationTest extends BaseStepDefinition {
 
@@ -48,18 +56,28 @@ public class BoardStepDefsIntegrationTest extends BaseStepDefinition {
             new Column(3L, thirdTitle, Collections.emptyList()))))));
   }
 
-  @Given("a user has previously accessed a board called {string}")
-  public void aUserHasPreviouslyAccessedABoardCalled(String boardName) {
+  @Given("a board exists called {string}")
+  public void aBoardExistsCalled(String title) {
+    // TODO post a real board when we have the endpoint for it
+  }
 
+  @Given("a user has previously accessed {string}")
+  public void aUserHasPreviouslyAccessed(String title) {
+    // TODO make a request to create a connection between user and board
   }
 
   @When("the user requests all their boards")
   public void theUserRequestsAllTheirBoards() {
-
+    HttpWrapper.executeGet(url + "/boards");
   }
 
   @Then("the user receives a list of the boards with one called {string}")
-  public void theUserReceivesAListOfTheBoardsWithOneCalled(String arg0) {
+  public void theUserReceivesAListOfTheBoardsWithOneCalled(String title)
+      throws JsonProcessingException {
+    List<Board> boards = new ObjectMapper().readValue(responseResult.getBody(), new TypeReference<>() {
+    });
 
+    assertThat(boards.size(), is(1));
+    assertThat(boards.get(0).getTitle(), is(title));
   }
 }
--- src/test/resources/features/board.feature
+++ src/test/resources/features/board.feature
@@ -7,6 +7,7 @@ Feature: board is retrieved
 
 Feature: all users boards are retrieved
   Scenario: user requests all boards
-    Given a user has previously accessed a board called "first sprint"
+    Given a board exists called "first sprint"
+    Given a user has previously accessed "first sprint"
     When the user requests all their boards
     Then the user receives a list of the boards with one called "first sprint"
