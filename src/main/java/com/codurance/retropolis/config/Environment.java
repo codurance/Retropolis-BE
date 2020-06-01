76892a1bed1494b9c42130810cc6834d46523791
Alexis Pavlidis <apavlidi@it.teithe.gr>
2020-06-02 02:18:43 +0300

    add dev environment configuration

--- src/main/java/com/codurance/retropolis/config/Environment.java
+++ src/main/java/com/codurance/retropolis/config/Environment.java
@@ -3,5 +3,6 @@ package com.codurance.retropolis.config;
 public class Environment {
 
   public static final String PROD = "prod";
+  public static final String DEV = "dev";
   public static final String TEST = "test";
 }
---
+++ src/main/java/com/codurance/retropolis/config/WebDevConfig.java
@@ -0,0 +1,24 @@
+package com.codurance.retropolis.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+@Profile(Environment.DEV)
+public class WebDevConfig {
+
+  @Configuration
+  public class WebConfiguration implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+      registry.addMapping("/**")
+          .allowedOrigins("http://localhost:3000");
+    }
+  }
+
+}
