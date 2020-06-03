009536b33c4c139235b44392d6a2f650dfe468c6
Tom <thomas.spencer@codurance.com>
2020-06-03 14:00:02 +0100

    Add interceptor

--- src/main/java/com/codurance/retropolis/config/WebDevConfig.java
+++ src/main/java/com/codurance/retropolis/config/WebDevConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
@@ -34,6 +35,10 @@ public class WebDevConfig {
           .allowedMethods("*")
           .allowedOrigins("http://localhost:3000");
     }
-  }
 
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+      registry.addInterceptor(new GoogleTokenAuthenticator());
+    }
+  }
 }
