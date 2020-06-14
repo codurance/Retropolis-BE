2b8548d28947318c2d371177aa382af4234f0e1b
Tom <thomas.spencer@codurance.com>
2020-06-14 15:10:24 +0100

    Add test for returning BoardResponseObject

--- src/test/java/com/codurance/retropolis/services/ApplicationBoardServiceTest.java
+++ src/test/java/com/codurance/retropolis/services/ApplicationBoardServiceTest.java
@@ -1,10 +1,16 @@
 package com.codurance.retropolis.services;
 
+import static java.util.Collections.emptyList;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.when;
 
+import com.codurance.retropolis.entities.Board;
 import com.codurance.retropolis.entities.User;
 import com.codurance.retropolis.exceptions.BoardNotFoundException;
+import com.codurance.retropolis.requests.NewBoardRequestObject;
+import com.codurance.retropolis.responses.BoardResponseObject;
 import com.codurance.retropolis.responses.BoardResponseObjectFactory;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -15,6 +21,7 @@ import org.mockito.junit.jupiter.MockitoExtension;
 @ExtendWith(MockitoExtension.class)
 public class ApplicationBoardServiceTest {
 
+  private static final String BOARD_TITLE = "test board";
   private final User USER = new User("john.doe@codurance.com", "John Doe");
   private final Long BOARD_ID = 1L;
   ApplicationBoardService applicationBoardService;
@@ -31,6 +38,26 @@ public class ApplicationBoardServiceTest {
   void setUp() {
     applicationBoardService = new ApplicationBoardService(userService, boardResponseFactory,
         boardService);
+  }
+
+  @Test
+  void returns_board_response_object() {
+    NewBoardRequestObject requestObject = new NewBoardRequestObject(BOARD_TITLE,
+        "john.doe@codurance.com");
+    requestObject.setUser(USER);
+    Board board = new Board(BOARD_ID, BOARD_TITLE, emptyList());
+    when(userService.findByEmail(USER.email)).thenReturn(USER);
+    when(boardService.getBoard(BOARD_ID)).thenReturn(board);
+
+    BoardResponseObject boardResponseObject = new BoardResponseObject(BOARD_ID, BOARD_TITLE,
+        emptyList());
+    when(boardResponseFactory.create(board, USER.getId())).thenReturn(boardResponseObject);
+
+    BoardResponseObject boardResponse = applicationBoardService.getBoard(USER, BOARD_ID);
+
+    assertEquals(BOARD_ID, boardResponse.getId());
+    assertEquals(BOARD_TITLE, boardResponse.getTitle());
+    assertEquals(0, boardResponse.getColumns().size());
   }
 
   @Test
