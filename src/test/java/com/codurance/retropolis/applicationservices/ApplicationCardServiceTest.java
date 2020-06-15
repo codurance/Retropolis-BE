5b3e3ce084574137d822524fa359d994c4b08dd5
Tom <thomas.spencer@codurance.com>
2020-06-15 17:18:00 +0100

    More tests for ApplicationCardServiceTest

--- src/test/java/com/codurance/retropolis/applicationservices/ApplicationCardServiceTest.java
+++ src/test/java/com/codurance/retropolis/applicationservices/ApplicationCardServiceTest.java
@@ -2,6 +2,7 @@ package com.codurance.retropolis.applicationservices;
 
 import static java.util.Collections.emptyList;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import com.codurance.retropolis.entities.Card;
@@ -10,8 +11,11 @@ import com.codurance.retropolis.services.CardService;
 import com.codurance.retropolis.services.UserService;
 import com.codurance.retropolis.web.requests.NewCardRequestObject;
 import com.codurance.retropolis.web.requests.UpVoteRequestObject;
+import com.codurance.retropolis.web.requests.UpdateCardRequestObject;
 import com.codurance.retropolis.web.responses.CardResponseObject;
 import com.codurance.retropolis.web.responses.CardResponseObjectFactory;
+import com.codurance.retropolis.web.responses.CardUpdatedTextResponseObject;
+import java.util.Collections;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -25,7 +29,6 @@ public class ApplicationCardServiceTest {
 
   private final Boolean HAVE_VOTED = false;
   private final String EMAIL = "john.doe@codurance.com";
-  private final String USERNAME = "John Doe";
   private final User USER = new User(3L, "john.doe@codurance.com", "John Doe");
   private final Long CARD_ID = 1L;
   private final Long COLUMN_ID = 2L;
@@ -76,14 +79,44 @@ public class ApplicationCardServiceTest {
   @Test
   void adds_upvote_to_card_response() {
     upvoteRequestObject = new UpVoteRequestObject(EMAIL, true);
-    ResponseEntity<HttpStatus> response = applicationCardService.addUpvote(CARD_ID, upvoteRequestObject);
-    assertEquals(200, response.getStatusCode().value());
+    ResponseEntity<HttpStatus> response = applicationCardService
+        .addUpvote(CARD_ID, upvoteRequestObject);
+    assertEquals(HttpStatus.OK, response.getStatusCode());
   }
 
   @Test
   void removes_upvote_from_card_response() {
     upvoteRequestObject = new UpVoteRequestObject(EMAIL, false);
-    ResponseEntity<HttpStatus> response = applicationCardService.removeUpvote(CARD_ID, upvoteRequestObject);
-    assertEquals(200, response.getStatusCode().value());
+    ResponseEntity<HttpStatus> response = applicationCardService
+        .removeUpvote(CARD_ID, upvoteRequestObject);
+    assertEquals(HttpStatus.OK, response.getStatusCode());
+  }
+
+  @Test
+  void deletes_card_with_id() {
+    ResponseEntity<HttpStatus> response = applicationCardService.delete(CARD_ID);
+    verify(cardService).delete(CARD_ID);
+    assertEquals(HttpStatus.OK, response.getStatusCode());
+  }
+
+  @Test
+  void updates_card_text() {
+    Card card = new Card(CARD_ID, TEXT, COLUMN_ID, USER.getId(), Collections.emptyList());
+    Card updatedCard = new Card(CARD_ID, "new text", COLUMN_ID, USER.getId(),
+        Collections.emptyList());
+    UpdateCardRequestObject updateCardRequest = new UpdateCardRequestObject();
+    when(cardService.updateText(CARD_ID, updateCardRequest)).thenReturn(updatedCard);
+
+    CardUpdatedTextResponseObject cardResponseObject = new CardUpdatedTextResponseObject(
+        updatedCard.getId(),
+        updatedCard.getText(),
+        updatedCard.getColumnId()
+    );
+
+    when(cardResponseObjectFactory.create(updatedCard)).thenReturn(cardResponseObject);
+
+    CardUpdatedTextResponseObject response = applicationCardService
+        .updateText(card.getId(), updateCardRequest);
+    assertEquals(cardResponseObject, response);
   }
 }
