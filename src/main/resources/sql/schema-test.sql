e4e8b52bef36d3163281392ffb348bb848cc2dee
Tom <thomas.spencer@codurance.com>
2020-06-16 14:35:42 +0100

    Format .sql files

--- src/main/resources/sql/schema-test.sql
+++ src/main/resources/sql/schema-test.sql
@@ -1,41 +1,51 @@
-CREATE TABLE IF NOT EXISTS users(
-                                    id       SERIAL PRIMARY KEY,
-                                    email    varchar UNIQUE,
-                                    username varchar
+CREATE TABLE IF NOT EXISTS users
+(
+    id       SERIAL PRIMARY KEY,
+    email    varchar UNIQUE,
+    username varchar
 );
 
-CREATE TABLE IF NOT EXISTS boards (
-  id SERIAL PRIMARY KEY,
-  title varchar
+CREATE TABLE IF NOT EXISTS boards
+(
+    id    SERIAL PRIMARY KEY,
+    title varchar
 );
 
-CREATE TABLE IF NOT EXISTS users_boards (
-  user_id int,
-  board_id int,
-  PRIMARY KEY(user_id, board_id),
-  FOREIGN KEY (user_id) REFERENCES users (id),
-  FOREIGN KEY (board_id) REFERENCES boards (id)
+CREATE TABLE IF NOT EXISTS users_boards
+(
+    user_id  int,
+    board_id int,
+    PRIMARY KEY (user_id, board_id),
+    FOREIGN KEY (user_id) REFERENCES users (id),
+    FOREIGN KEY (board_id) REFERENCES boards (id)
 );
 
-CREATE TABLE IF NOT EXISTS columns (
-  id SERIAL PRIMARY KEY,
-  title varchar,
-  board_id int,
-  FOREIGN KEY (board_id) REFERENCES boards (id)
+CREATE TABLE IF NOT EXISTS columns
+(
+    id       SERIAL PRIMARY KEY,
+    title    varchar,
+    board_id int,
+    FOREIGN KEY (board_id) REFERENCES boards (id)
 );
 
-CREATE TABLE IF NOT EXISTS cards (
-  id SERIAL PRIMARY KEY,
-  text varchar,
-  user_id int,
-  column_id int,
-  voters int[],
-  FOREIGN KEY (column_id) REFERENCES columns (id),
-  FOREIGN KEY (user_id) REFERENCES users (id)
+CREATE TABLE IF NOT EXISTS cards
+(
+    id        SERIAL PRIMARY KEY,
+    text      varchar,
+    user_id   int,
+    column_id int,
+    voters    int[],
+    FOREIGN KEY (column_id) REFERENCES columns (id),
+    FOREIGN KEY (user_id) REFERENCES users (id)
 );
 
-insert into boards(id, title) values(1, 'test board');
-insert into columns(title, board_id) values('Start', 1);
-insert into columns(title, board_id) values('Stop', 1);
-insert into columns(title, board_id) values('Continue', 1);
-insert into users(email, username) values('john.doe@codurance.com', 'john doe');
+insert into boards(id, title)
+values (1, 'test board');
+insert into columns(title, board_id)
+values ('Start', 1);
+insert into columns(title, board_id)
+values ('Stop', 1);
+insert into columns(title, board_id)
+values ('Continue', 1);
+insert into users(email, username)
+values ('john.doe@codurance.com', 'john doe');
--- src/main/resources/sql/schema.sql
+++ src/main/resources/sql/schema.sql
@@ -1,35 +1,40 @@
-CREATE TABLE IF NOT EXISTS users(
-                                    id       SERIAL PRIMARY KEY,
-                                    email    varchar UNIQUE,
-                                    username varchar
+CREATE TABLE IF NOT EXISTS users
+(
+    id       SERIAL PRIMARY KEY,
+    email    varchar UNIQUE,
+    username varchar
 );
 
-CREATE TABLE IF NOT EXISTS boards (
-  id SERIAL PRIMARY KEY,
-  title varchar
+CREATE TABLE IF NOT EXISTS boards
+(
+    id    SERIAL PRIMARY KEY,
+    title varchar
 );
 
-CREATE TABLE IF NOT EXISTS users_boards (
-  user_id int,
-  board_id int,
-  PRIMARY KEY(user_id, board_id),
-  FOREIGN KEY (user_id) REFERENCES users (id),
-  FOREIGN KEY (board_id) REFERENCES boards (id)
+CREATE TABLE IF NOT EXISTS users_boards
+(
+    user_id  int,
+    board_id int,
+    PRIMARY KEY (user_id, board_id),
+    FOREIGN KEY (user_id) REFERENCES users (id),
+    FOREIGN KEY (board_id) REFERENCES boards (id)
 );
 
-CREATE TABLE IF NOT EXISTS columns (
-  id SERIAL PRIMARY KEY,
-  title varchar,
-  board_id int,
-  FOREIGN KEY (board_id) REFERENCES boards (id)
+CREATE TABLE IF NOT EXISTS columns
+(
+    id       SERIAL PRIMARY KEY,
+    title    varchar,
+    board_id int,
+    FOREIGN KEY (board_id) REFERENCES boards (id)
 );
 
-CREATE TABLE IF NOT EXISTS cards (
-  id SERIAL PRIMARY KEY,
-  text varchar,
-  user_id int,
-  column_id int,
-  voters int[],
-  FOREIGN KEY (column_id) REFERENCES columns (id),
-  FOREIGN KEY (user_id) REFERENCES users (id)
+CREATE TABLE IF NOT EXISTS cards
+(
+    id        SERIAL PRIMARY KEY,
+    text      varchar,
+    user_id   int,
+    column_id int,
+    voters    int[],
+    FOREIGN KEY (column_id) REFERENCES columns (id),
+    FOREIGN KEY (user_id) REFERENCES users (id)
 );
